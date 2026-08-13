d5d6d26e23fae6f86ba3e6595b9b7ad7a4ffd49d
meynoik <neynoi.k@gmail.com>
2026-08-13 20:38:21 +0200

    Add NeoForge mod entrypoint

---
+++ src/main/java/atomicstryker/battletowers/BattleTowers.java
@@ -0,0 +1,17 @@
+package atomicstryker.battletowers;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(BattleTowers.MOD_ID)
+public final class BattleTowers {
+    public static final String MOD_ID = "battletowers";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public BattleTowers(IEventBus modEventBus, ModContainer modContainer) {
+        LOGGER.info("Battle Towers NeoForge 1.21.1 port scaffold loaded");
+    }
+}
