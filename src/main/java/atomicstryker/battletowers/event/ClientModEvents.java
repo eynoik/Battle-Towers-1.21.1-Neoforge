d1468757a5ed8b3ea0932f9b400c4e8fdca51e65
meynoik <neynoi.k@gmail.com>
2026-08-14 15:38:02 +0200

    Register Battle Tower Golem model layer

--- src/main/java/atomicstryker/battletowers/event/ClientModEvents.java
+++ src/main/java/atomicstryker/battletowers/event/ClientModEvents.java
@@ -1,6 +1,7 @@
 package atomicstryker.battletowers.event;
 
 import atomicstryker.battletowers.BattleTowers;
+import atomicstryker.battletowers.client.BattleTowerGolemModel;
 import atomicstryker.battletowers.client.BattleTowerGolemRenderer;
 import atomicstryker.battletowers.registry.ModEntities;
 import net.neoforged.api.distmarker.Dist;
@@ -11,6 +12,11 @@ import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 @EventBusSubscriber(modid = BattleTowers.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class ClientModEvents {
     private ClientModEvents() {
+    }
+
+    @SubscribeEvent
+    public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(BattleTowerGolemModel.LAYER, BattleTowerGolemModel::createBodyLayer);
     }
 
     @SubscribeEvent
