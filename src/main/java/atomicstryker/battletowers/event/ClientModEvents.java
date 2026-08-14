3e1074b4709caa801b92ac6b74c0cbd37d9a7935
meynoik <neynoi.k@gmail.com>
2026-08-14 11:08:58 +0200

    Use classic Golem renderer without raw casts

--- src/main/java/atomicstryker/battletowers/event/ClientModEvents.java
+++ src/main/java/atomicstryker/battletowers/event/ClientModEvents.java
@@ -1,10 +1,8 @@
 package atomicstryker.battletowers.event;
 
 import atomicstryker.battletowers.BattleTowers;
-import atomicstryker.battletowers.entity.BattleTowerGolem;
+import atomicstryker.battletowers.client.BattleTowerGolemRenderer;
 import atomicstryker.battletowers.registry.ModEntities;
-import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.IronGolemRenderer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -12,12 +10,11 @@ import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
 @EventBusSubscriber(modid = BattleTowers.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class ClientModEvents {
-    private ClientModEvents() {}
+    private ClientModEvents() {
+    }
 
     @SubscribeEvent
-    @SuppressWarnings({"rawtypes", "unchecked"})
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        event.registerEntityRenderer(ModEntities.BATTLE_TOWER_GOLEM.get(), context ->
-                (EntityRenderer<BattleTowerGolem>) (EntityRenderer) new IronGolemRenderer(context));
+        event.registerEntityRenderer(ModEntities.BATTLE_TOWER_GOLEM.get(), BattleTowerGolemRenderer::new);
     }
 }
