f8acdf7093a0128a13eb05a072a15311783a6c44
meynoik <neynoi.k@gmail.com>
2026-08-14 16:15:24 +0200

    Remove Battle Tower floor lights

--- src/main/java/atomicstryker/battletowers/world/TowerFloorPopulator.java
+++ src/main/java/atomicstryker/battletowers/world/TowerFloorPopulator.java
@@ -1,23 +1,19 @@
 package atomicstryker.battletowers.world;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.WallTorchBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
 
 final class TowerFloorPopulator {
     private TowerFloorPopulator() {
     }
 
     static void populate(ServerLevel level, BlockPos floorOrigin, TowerType type, int floorIndex, int floorCount, boolean underground) {
-        placeLights(level, floorOrigin, type);
         placeLoot(level, floorOrigin, type, floorIndex, floorCount, underground);
 
         boolean bossFloor = underground ? floorIndex == 0 : floorIndex == floorCount - 1;
@@ -25,22 +21,6 @@ final class TowerFloorPopulator {
             placeSpawner(level, floorOrigin.offset(2, 6, 2), selectMob(level.random));
             placeSpawner(level, floorOrigin.offset(-3, 6, 2), selectMob(level.random));
         }
-    }
-
-    private static void placeLights(ServerLevel level, BlockPos origin, TowerType type) {
-        if (type.light() == Blocks.AIR) {
-            return;
-        }
-
-        BlockState light = type.light().defaultBlockState();
-        if (type.light() == Blocks.TORCH) {
-            light = Blocks.WALL_TORCH.defaultBlockState().setValue(WallTorchBlock.FACING, Direction.SOUTH);
-        }
-
-        level.setBlock(origin.offset(3, 2, -6), light, 2);
-        level.setBlock(origin.offset(-4, 2, -6), light, 2);
-        level.setBlock(origin.offset(1, 2, -4), light, 2);
-        level.setBlock(origin.offset(-2, 2, -4), light, 2);
     }
 
     private static void placeLoot(ServerLevel level, BlockPos origin, TowerType type, int floorIndex, int floorCount, boolean underground) {
