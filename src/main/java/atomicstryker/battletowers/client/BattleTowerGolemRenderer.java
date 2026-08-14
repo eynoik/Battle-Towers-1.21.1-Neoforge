7f8f772db56ad7b2c31edc23b6d12e57ea3309b4
meynoik <neynoi.k@gmail.com>
2026-08-14 15:37:51 +0200

    Use legacy-compatible Golem model layer

--- src/main/java/atomicstryker/battletowers/client/BattleTowerGolemRenderer.java
+++ src/main/java/atomicstryker/battletowers/client/BattleTowerGolemRenderer.java
@@ -3,18 +3,16 @@ package atomicstryker.battletowers.client;
 import atomicstryker.battletowers.BattleTowers;
 import atomicstryker.battletowers.entity.BattleTowerGolem;
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
-public final class BattleTowerGolemRenderer extends MobRenderer<BattleTowerGolem, HumanoidModel<BattleTowerGolem>> {
+public final class BattleTowerGolemRenderer extends MobRenderer<BattleTowerGolem, BattleTowerGolemModel> {
     private static final ResourceLocation AWAKE = ResourceLocation.fromNamespaceAndPath(BattleTowers.MOD_ID, "textures/model/golem.png");
     private static final ResourceLocation DORMANT = ResourceLocation.fromNamespaceAndPath(BattleTowers.MOD_ID, "textures/model/golemdormant.png");
 
     public BattleTowerGolemRenderer(EntityRendererProvider.Context context) {
-        super(context, new HumanoidModel<>(context.bakeLayer(ModelLayers.PLAYER)), 1.0F);
+        super(context, new BattleTowerGolemModel(context.bakeLayer(BattleTowerGolemModel.LAYER)), 1.0F);
     }
 
     @Override
