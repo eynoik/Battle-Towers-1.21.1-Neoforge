3e13959e20d1ea7ed29eded75e0dc826aecadcd3
meynoik <neynoi.k@gmail.com>
2026-08-14 10:24:58 +0200

    Spawn Battle Tower Golem on completed towers

---
+++ src/main/java/atomicstryker/battletowers/world/TowerBossSpawner.java
@@ -0,0 +1,24 @@
+package atomicstryker.battletowers.world;
+
+import atomicstryker.battletowers.entity.BattleTowerGolem;
+import atomicstryker.battletowers.registry.ModEntities;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+
+final class TowerBossSpawner {
+    private TowerBossSpawner() {
+    }
+
+    static void spawn(ServerLevel level, BlockPos towerOrigin, BlockPos bossPos, TowerType type) {
+        BattleTowerGolem golem = ModEntities.BATTLE_TOWER_GOLEM.get().create(level);
+        if (golem == null) {
+            return;
+        }
+
+        golem.setTowerType(type.legacyId());
+        golem.setTowerOrigin(towerOrigin);
+        golem.moveTo(bossPos.getX() + 0.5D, bossPos.getY(), bossPos.getZ() + 0.5D, level.random.nextFloat() * 360.0F, 0.0F);
+        golem.setDormant();
+        level.addFreshEntity(golem);
+    }
+}
